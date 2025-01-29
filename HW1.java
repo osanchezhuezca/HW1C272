9f086d7c6da1d10f3251a8f16425760391ae7f82
osanchezhuezca <150964864+osanchezhuezca@users.noreply.github.com>
2025-01-29 11:43:22 -0600

    Update HW1.java

--- HW1.java
+++ HW1.java
@@ -1,6 +1,6 @@
 
 /*
- * *** PLACE YOUR NAME / SECTION  HERE ***
+ * *** Oscar Sanchez Huezca / Section 001 ***
  *
  * Homework # 1 (Programming Assignment). This Java class defines some basic
  * manipulation operations on Linked-Lists and Stacks.
